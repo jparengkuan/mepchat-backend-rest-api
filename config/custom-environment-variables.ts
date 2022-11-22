f15af67ce0d5557fb024fcdce88dc3ba34c9d6ae
FJ Parengkuan <f.j.parengkuan@st.hanze.nl>
2022-11-22 20:09:03 +0100

    Middleware

--- config/custom-environment-variables.ts
+++ config/custom-environment-variables.ts
@@ -1,6 +1,8 @@
 export default {
+    dbHost: 'MONGODB_HOSTNAME',
     dbName: 'MONGODB_USERNAME',
     dbPass: 'MONGODB_PASSWORD',
+    redisHost: 'REDIS_HOSTNAME',
     accessTokenPrivateKey: 'ACCESS_TOKEN_PRIVATE_KEY',
     accessTokenPublicKey: 'ACCESS_TOKEN_PUBLIC_KEY',
 };
