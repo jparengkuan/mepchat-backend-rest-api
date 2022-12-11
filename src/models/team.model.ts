48faaabb511e61de2fe7ef7dca0f8c31af68646c
FJ Parengkuan <f.j.parengkuan@st.hanze.nl>
2022-12-11 19:41:07 +0100

    teammodel now referencing the usermodel correctly

--- src/models/team.model.ts
+++ src/models/team.model.ts
@@ -1,15 +1,13 @@
-import { getModelForClass, index, prop } from "@typegoose/typegoose";
+import { getModelForClass, prop, Ref} from "@typegoose/typegoose";
 import { User } from "./user.model";
-
-@index({ name: 1})
 
 // Export the Team class to be used as Typescript type
 export class Team {
     @prop({ unique: true, required: true })
     name: string;
 
-    @prop()
-    users?: User
+    @prop({ ref: () => User })
+    users: Ref<User>[];
 }
 
 // Create the team model from the Team class
