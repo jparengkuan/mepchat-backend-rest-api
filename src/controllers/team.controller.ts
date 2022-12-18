61fdfe003021938a692429dbd8602b0acdb4ccda
FJ Parengkuan <f.j.parengkuan@st.hanze.nl>
2022-12-18 16:45:40 +0100

    Gebruikers kunnen worden toegevoegt aan een team

--- src/controllers/team.controller.ts
+++ src/controllers/team.controller.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
-import { CreateTeamInput } from '../schema/team.schema';
-import { createTeam } from '../services/team.service';
+import { addUserToTeamInput, CreateTeamInput } from '../schema/team.schema';
+import { addUser, createTeam, findTeamByName } from '../services/team.service';
+import { findUserById } from '../services/user.service';
 
 export const newTeamHandler = async (
   req: Request<{}, {}, CreateTeamInput>,
@@ -25,6 +26,55 @@ export const newTeamHandler = async (
         message: 'Team already exist',
       });
     }
+    next(err);
+  }
+};
+
+export const addUserToTeam = async (
+  req: Request<{}, {}, addUserToTeamInput>,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    // First check if the given team exist
+    const team = await findTeamByName(req.body.teamName)
+
+    // Return 404 if team doest not exists
+    if (!team) {
+      return res.status(404).json({
+        status: 'fail',
+        message: 'Team does not exist',
+      });
+    }
+
+    // Second check if the given user exist
+    try {
+      const user = await findUserById(req.body.userId)
+
+      const updatedTeam = await addUser(req.body.teamName, user)
+
+      res.status(201).json({
+        status: 'success',
+        data: {
+          updatedTeam,
+        },
+      });
+    }
+    catch ( err: any)
+    {
+      if (err.kind == 'ObjectId'){
+        res.status(400).json({
+          status: 'fail',
+          message: "userId is not valid"
+        });
+      }
+    }
+    
+
+  } catch (err: any) {
+
+    console.log(err)
+
     next(err);
   }
 };
